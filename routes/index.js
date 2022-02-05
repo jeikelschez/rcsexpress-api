4bbe91fdba55ec1efc914f516c2013ccced8aecb
Jeikel Sanchez <jeikelschez@hotmail.com>
2022-02-05 15:29:12 -0400

    Ruteo swagger API

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,9 @@ const BancosRouter = require('./bancos.router');
 function routerApi(app) {
   const router = express.Router();
 
-  app.use('/api', router);
-  router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
   app.use('/api/v1', router);
   router.use('/bancos', BancosRouter);
+  router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
 module.exports = routerApi;
