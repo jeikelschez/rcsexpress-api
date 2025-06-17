f60a5cdcd405e7c5ed7b0b6edaff61eaba31248c
jeikel <jeikel.sanchez@quanticvision.com>
2025-06-17 08:05:14 -0400

    Correcciones de formato resportes de venta

--- services/pdfReports/reporteVentas.service.js
+++ services/pdfReports/reporteVentas.service.js
@@ -541,6 +541,11 @@ class ReporteVentasService {
             ventasAgrArray.cliente =
               ventas[ventas.length - 1]['clientes_org.nb_cliente'];
         }
+        ventasAgrArray.agencia =
+          ventas[ventas.length - 1]['agencias.nb_agencia'];
+        ventasAgrArray.sort(
+          (a, b) => parseFloat(b.monto_total) - parseFloat(a.monto_total)
+        );
         ventas = ventasAgrArray;
         break;
       case 'GC':
@@ -1038,7 +1043,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
         doc.y = 100;
         doc.x = 285;
@@ -1104,7 +1109,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
         doc.y = 90;
         doc.x = 285;
@@ -1184,14 +1189,13 @@ class ReporteVentasService {
         } else if (tipo == 'RD') {
           labelDoc = ' Relación de Despacho para las Agencias';
         }
-        doc.image('./img/logo_rc.png', 35, 25, { width: 80 });
-        doc.fontSize(9);
-        doc.text('RCS EXPRESS, S.A', 35, 155);
-        doc.text('RIF. J-31028463-6', 35, 170);
+        doc.image('./img/logo_rc.png', 30, 25, { width: 50 });
+        doc.fontSize(8);
+        doc.text('RCS EXPRESS, S.A', 30, 105);
+        doc.text('RIF. J-31028463-6', 30, 115);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
         doc.fontSize(18);
-        doc.y = 90;
+        doc.y = 60;
         doc.x = 235;
         doc.text(labelDoc, {
           align: 'center',
@@ -1199,30 +1203,26 @@ class ReporteVentasService {
           width: 400,
         });
         doc.fontSize(12);
-        doc.y = 115;
-        doc.x = 320;
-        doc.text('Desde: ' + data.fecha_desde, {
-          align: 'left',
-          columns: 1,
-          width: 300,
-        });
-        doc.y = 115;
-        doc.x = 440;
-        doc.text('Hasta: ' + data.fecha_hasta, {
-          align: 'left',
-          columns: 1,
-          width: 300,
-        });
+        doc.y = 82;
+        doc.x = 235;
+        doc.text(
+          'Desde: ' + data.fecha_desde + '     ' + 'Hasta: ' + data.fecha_hasta,
+          {
+            align: 'center',
+            columns: 1,
+            width: 400,
+          }
+        );
 
         if (tipo == 'VCM' || tipo == 'VCD') {
-          doc.y = 135;
+          doc.y = 99;
           doc.x = 235;
           doc.text('Agencia: ' + data.ventas.agencia, {
             align: 'center',
             columns: 1,
             width: 400,
           });
-          doc.y = 150;
+          doc.y = 114;
           doc.x = 235;
           doc.text('Cliente: ' + data.ventas.cliente, {
             align: 'center',
@@ -1231,11 +1231,11 @@ class ReporteVentasService {
           });
         }
 
-        if (tipo == 'RD') {
+        if (tipo == 'RD' || tipo == 'TVD' || tipo == 'TVC') {
           if (data.agencia) {
-            doc.y = 135;
+            doc.y = 99;
             doc.x = 235;
-            doc.text('Agencia: ' + data.ventas.agencia, {
+            doc.text('Origen: ' + data.ventas.agencia, {
               align: 'center',
               columns: 1,
               width: 400,
@@ -1243,7 +1243,7 @@ class ReporteVentasService {
           }
 
           if (data.cliente) {
-            doc.y = 150;
+            doc.y = 114;
             doc.x = 235;
             doc.text('Cliente: ' + data.ventas.cliente, {
               align: 'center',
@@ -1264,32 +1264,36 @@ class ReporteVentasService {
           labelFirst = 'Cliente';
         }
 
+        doc.lineCap('butt').moveTo(280, 145).lineTo(455, 145).stroke();
+        doc.lineCap('butt').moveTo(465, 145).lineTo(545, 145).stroke();
+        doc.lineCap('butt').moveTo(30, 163).lineTo(760, 163).stroke();
+
         doc.fontSize(9);
         doc.text('Fecha: ' + moment().format('DD/MM/YYYY'), 670, 35);
-        doc.text(labelFirst, tipo != 'TVC' ? 50 : 70, 190);
+        doc.text(labelFirst, tipo != 'TVC' ? 50 : 70, 150);
         doc.text(
           'Guías',
-          tipo == 'TVC' || tipo == 'TV' || tipo == 'RD' ? 150 : 120,
-          190
+          tipo == 'TVC' || tipo == 'TV' || tipo == 'RD' ? 175 : 145,
+          150
         );
         doc.text(
           'Pzas',
-          tipo == 'TVC' || tipo == 'TV' || tipo == 'RD' ? 190 : 180,
-          190
+          tipo == 'TVC' || tipo == 'TV' || tipo == 'RD' ? 210 : 200,
+          150
         );
-        doc.text(data.neta ? 'Neta' : 'Kgs', 230, 190);
-        doc.text('CONTADO', 330, 178);
-        doc.text('Origen', 270, 190);
-        doc.text('Imp.', 320, 190);
-        doc.text('Destino', 370, 190);
-        doc.text('Imp.', 420, 190);
-        doc.text('CRÉDITO', 485, 178);
-        doc.text('Monto', 470, 190);
-        doc.text('Imp.', 520, 190);
-        doc.text('Otros.', 560, 190);
-        if (data.dolar == true) doc.text('Otr $.', 600, 190);
-        doc.text('Total Venta', 645, 190);
-        if (data.dolar == true) doc.text('Venta $.', 720, 190);
+        doc.text(data.neta ? 'Neta' : 'Kgs', 255, 150);
+        doc.text('CONTADO', 340, 135);
+        doc.text('Origen', 290, 150);
+        doc.text('Imp.', 340, 150);
+        doc.text('Destino', 380, 150);
+        doc.text('Imp.', 430, 150);
+        doc.text('CRÉDITO', 485, 135);
+        doc.text('Monto', 480, 150);
+        doc.text('Imp.', 520, 150);
+        doc.text('Otros.', 560, 150);
+        if (data.dolar == true) doc.text('Otr $.', 600, 150);
+        doc.text('Total Venta', 645, 150);
+        if (data.dolar == true) doc.text('Venta $.', 720, 150);
         break;
       case 'GC':
       case 'FA':
@@ -1299,7 +1303,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
 
         let tittle = 'Guías Cargas';
@@ -1399,7 +1403,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
 
         doc.y = 100;
@@ -1457,7 +1461,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
 
         doc.y = 100;
@@ -1518,7 +1522,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
 
         doc.y = 100;
@@ -1573,7 +1577,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
 
         doc.y = 100;
@@ -1629,7 +1633,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
 
         doc.y = 100;
@@ -1672,7 +1676,7 @@ class ReporteVentasService {
         doc.text('RCS EXPRESS, S.A', 35, 155);
         doc.text('RIF. J-31028463-6', 35, 170);
         doc.font('Helvetica-Bold');
-        doc.fillColor('#444444');
+
         doc.fontSize(18);
 
         doc.y = 100;
@@ -1774,7 +1778,7 @@ class ReporteVentasService {
           }
           doc.fontSize(6);
           doc.font('Helvetica');
-          doc.fillColor('#444444');
+
           doc.y = ymin + i;
           doc.x = 20;
           doc.text(item + 1, {
@@ -2338,7 +2342,7 @@ class ReporteVentasService {
         for (var item = 0; item < data.ventas.length; item++) {
           doc.fontSize(6);
           doc.font('Helvetica');
-          doc.fillColor('#444444');
+
           doc.y = ymin + i;
           doc.x = 20;
           doc.text(item + 1, {
@@ -2667,14 +2671,11 @@ class ReporteVentasService {
       case 'RD':
         var i = 0;
         var page = 0;
-        var ymin;
-        ymin = 210;
+        var ymin = 170;
         for (var item = 0; item < data.ventas.length; item++) {
           doc.font('Helvetica');
-          doc.fillColor('#444444');
-
+          doc.fontSize(8);
           if (tipo == 'VCM' || tipo == 'VCD' || tipo == 'TVD') {
-            doc.fontSize(8);
             doc.y = ymin + i;
             doc.x = tipo == 'VCM' ? 35 : 47;
             doc.text(
@@ -2688,34 +2689,6 @@ class ReporteVentasService {
               }
             );
             doc.y = ymin + i;
-            doc.x = 90;
-            doc.text(data.ventas[item].nro_guias, {
-              align: 'right',
-              columns: 1,
-              width: 60,
-            });
-            doc.y = ymin + i;
-            doc.x = 145;
-            doc.text(data.ventas[item].nro_piezas, {
-              align: 'right',
-              columns: 1,
-              width: 60,
-            });
-          } else if (tipo == 'TV' || tipo == 'TVC' || tipo == 'RD') {
-            doc.fontSize(7);
-            doc.y = ymin + i;
-            doc.x = 30;
-            doc.text(
-              tipo == 'TV' || tipo == 'RD'
-                ? data.ventas[item].agencia
-                : data.ventas[item].cliente,
-              {
-                align: 'left',
-                columns: 1,
-                width: 130,
-              }
-            );
-            doc.y = ymin + i;
             doc.x = 115;
             doc.text(data.ventas[item].nro_guias, {
               align: 'right',
@@ -2723,7 +2696,35 @@ class ReporteVentasService {
               width: 60,
             });
             doc.y = ymin + i;
-            doc.x = 150;
+            doc.x = 165;
+            doc.text(data.ventas[item].nro_piezas, {
+              align: 'right',
+              columns: 1,
+              width: 60,
+            });
+          } else if (tipo == 'TV' || tipo == 'TVC' || tipo == 'RD') {
+            doc.y = ymin + i;
+            doc.x = 30;
+            doc.text(
+              tipo == 'TV' || tipo == 'RD'
+                ? data.ventas[item].agencia
+                : data.ventas[item].cliente.substr(0, 32),
+              {
+                align: 'left',
+                columns: 1,
+                width: 160,
+                lineGap: -1.5, // Puedes probar con valores negativos, por ejemplo -2
+              }
+            );
+            doc.y = ymin + i;
+            doc.x = 140;
+            doc.text(data.ventas[item].nro_guias, {
+              align: 'right',
+              columns: 1,
+              width: 60,
+            });
+            doc.y = ymin + i;
+            doc.x = 170;
             doc.text(data.ventas[item].nro_piezas, {
               align: 'right',
               columns: 1,
@@ -2736,7 +2737,7 @@ class ReporteVentasService {
             : data.ventas[item].peso_kgs;
 
           doc.y = ymin + i;
-          doc.x = 195;
+          doc.x = 220;
           doc.text(utils.formatNumber(monto_kgs), {
             align: 'right',
             columns: 1,
@@ -2745,28 +2746,28 @@ class ReporteVentasService {
 
           if (data.visible == 'SI') {
             doc.y = ymin + i;
-            doc.x = 250;
+            doc.x = 270;
             doc.text(utils.formatNumber(data.ventas[item].monto_contado_orig), {
               align: 'right',
               columns: 1,
               width: 50,
             });
             doc.y = ymin + i;
-            doc.x = 290;
+            doc.x = 310;
             doc.text(utils.formatNumber(data.ventas[item].imp_contado_orig), {
               align: 'right',
               columns: 1,
               width: 50,
             });
             doc.y = ymin + i;
-            doc.x = 355;
+            doc.x = 365;
             doc.text(utils.formatNumber(data.ventas[item].monto_contado_dest), {
               align: 'right',
               columns: 1,
               width: 50,
             });
             doc.y = ymin + i;
-            doc.x = 390;
+            doc.x = 400;
             doc.text(utils.formatNumber(data.ventas[item].imp_contado_dest), {
               align: 'right',
               columns: 1,
@@ -2774,7 +2775,7 @@ class ReporteVentasService {
             });
 
             doc.y = ymin + i;
-            doc.x = 450;
+            doc.x = 460;
             doc.text(utils.formatNumber(data.ventas[item].monto_credito_orig), {
               align: 'right',
               columns: 1,
@@ -2861,7 +2862,7 @@ class ReporteVentasService {
             data.ventas[item].monto_total_dolar
           );
 
-          i += tipo == 'TVC' || tipo == 'RD' ? 20 : 15;
+          i += 15;
           if (i >= 370) {
             doc.fillColor('#BLACK');
             doc.addPage();
@@ -2872,8 +2873,10 @@ class ReporteVentasService {
           }
         }
 
+        doc.lineCap('butt').moveTo(30, ymin + i).lineTo(760, ymin + i).stroke();
+
         // Totales Finales
-        i += 15;
+        i += 10;
         doc.font('Helvetica-Bold');
         doc.y = ymin + i;
         doc.x = tipo == 'VCM' || tipo == 'VCD' ? 70 : 90;
@@ -2883,14 +2886,14 @@ class ReporteVentasService {
           width: 100,
         });
         doc.y = ymin + i;
-        doc.x = tipo == 'VCM' || tipo == 'VCD' ? 90 : 115;
+        doc.x = tipo == 'VCM' || tipo == 'VCD' ? 115 : 140;
         doc.text(totalNroGuias, {
           align: 'right',
           columns: 1,
           width: 60,
         });
         doc.y = ymin + i;
-        doc.x = tipo == 'VCM' || tipo == 'VCD' ? 145 : 150;
+        doc.x = tipo == 'VCM' || tipo == 'VCD' ? 165 : 170;
         doc.text(totalNroPiezas, {
           align: 'right',
           columns: 1,
@@ -2898,7 +2901,7 @@ class ReporteVentasService {
         });
 
         doc.y = ymin + i;
-        doc.x = 195;
+        doc.x = 220;
         doc.text(utils.formatNumber(totalPesoKgs), {
           align: 'right',
           columns: 1,
@@ -2907,28 +2910,28 @@ class ReporteVentasService {
 
         if (data.visible == 'SI') {
           doc.y = ymin + i;
-          doc.x = 250;
+          doc.x = 270;
           doc.text(utils.formatNumber(totalContadoOrig), {
             align: 'right',
             columns: 1,
             width: 50,
           });
           doc.y = ymin + i;
-          doc.x = 290;
+          doc.x = 310;
           doc.text(utils.formatNumber(totalImpContadoOrig), {
             align: 'right',
             columns: 1,
             width: 50,
           });
           doc.y = ymin + i;
-          doc.x = 355;
+          doc.x = 365;
           doc.text(utils.formatNumber(totalContadoDest), {
             align: 'right',
             columns: 1,
             width: 50,
           });
           doc.y = ymin + i;
-          doc.x = 390;
+          doc.x = 400;
           doc.text(utils.formatNumber(totalImpContadoDest), {
             align: 'right',
             columns: 1,
@@ -2936,7 +2939,7 @@ class ReporteVentasService {
           });
 
           doc.y = ymin + i;
-          doc.x = 450;
+          doc.x = 460;
           doc.text(utils.formatNumber(totalCreditoOrig), {
             align: 'right',
             columns: 1,
@@ -3016,7 +3019,7 @@ class ReporteVentasService {
 
               doc.fontSize(7);
               doc.font('Helvetica');
-              doc.fillColor('#444444');
+
               doc.y = ymin + i;
               doc.x = 25;
               doc.text(
@@ -3318,7 +3321,7 @@ class ReporteVentasService {
 
               doc.fontSize(7);
               doc.font('Helvetica');
-              doc.fillColor('#444444');
+
               doc.y = ymin + i;
               doc.x = 25;
               doc.text(
@@ -3639,7 +3642,7 @@ class ReporteVentasService {
 
             doc.fontSize(7);
             doc.font('Helvetica');
-            doc.fillColor('#444444');
+
             doc.y = ymin + i;
             doc.x = 25;
             doc.text(
@@ -3879,7 +3882,7 @@ class ReporteVentasService {
         for (var item = 0; item < data.ventas.length; item++) {
           doc.fontSize(7);
           doc.font('Helvetica');
-          doc.fillColor('#444444');
+
           doc.y = ymin + i;
           doc.x = 25;
           doc.text(
@@ -4100,7 +4103,7 @@ class ReporteVentasService {
 
           doc.fontSize(7);
           doc.font('Helvetica');
-          doc.fillColor('#444444');
+
           doc.y = ymin + i;
           doc.x = 25;
           doc.text(
@@ -4437,7 +4440,7 @@ class ReporteVentasService {
 
           doc.fontSize(8);
           doc.font('Helvetica');
-          doc.fillColor('#444444');
+
           doc.y = ymin + i;
           doc.x = 25;
           doc.text(
@@ -4685,7 +4688,7 @@ class ReporteVentasService {
 
           doc.fontSize(7);
           doc.font('Helvetica');
-          doc.fillColor('#444444');
+
           doc.y = ymin + i;
           doc.x = 20;
           doc.text(
@@ -4883,10 +4886,9 @@ class ReporteVentasService {
         var page = 0;
         var ymin;
         ymin = 210;
-        for (var item = 0; item < data.ventas.length; item++) { 
+        for (var item = 0; item < data.ventas.length; item++) {
           doc.fontSize(8);
           doc.font('Helvetica');
-          doc.fillColor('#444444');
 
           doc.y = ymin + i;
           doc.x = 35;
@@ -4973,7 +4975,7 @@ class ReporteVentasService {
             columns: 1,
             width: 60,
           });
-          
+
           totalSubtotal += utils.parseFloatN(montoSubtotal);
           totalImpuesto += utils.parseFloatN(montoImpuesto);
           totalMontoVenta += utils.parseFloatN(montoTotal);
@@ -5035,10 +5037,9 @@ class ReporteVentasService {
         var page = 0;
         var ymin;
         ymin = 210;
-        for (var item = 0; item < data.ventas.length; item++) { 
+        for (var item = 0; item < data.ventas.length; item++) {
           doc.fontSize(9);
           doc.font('Helvetica');
-          doc.fillColor('#444444');          
 
           let nroDocumento = data.ventas[item].t_de_documento + ' ';
           if (!data.ventas[item].nro_control) {
@@ -5117,7 +5118,7 @@ class ReporteVentasService {
             columns: 1,
             width: 60,
           });
-          
+
           totalSubtotal += utils.parseFloatN(montoSubtotal);
           totalImpuesto += utils.parseFloatN(montoImpuesto);
           totalMontoVenta += utils.parseFloatN(montoTotal);
@@ -5186,7 +5187,7 @@ class ReporteVentasService {
     ) {
       doc.switchToPage(i);
       doc.fontSize(8);
-      doc.fillColor('#444444');
+
       doc.x =
         tipo == 'GC' ||
         tipo == 'FA' ||
@@ -5200,7 +5201,7 @@ class ReporteVentasService {
         tipo == 'CCC'
           ? 485
           : 646;
-      doc.y = 50;
+      doc.y = 46;
       doc.text(`Pagina ${i + 1} de ${range.count}`, {
         align: 'right',
         columns: 1,
